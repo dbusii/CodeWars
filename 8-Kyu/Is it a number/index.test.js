6b594d30600d1a4cf56ee07de03180a42df83688
dbusii <dtbirkett@gmail.com>
2019-04-22 22:37:58 +0100

    Codewars kata 8-Kyu

---
+++ 8-Kyu/Is it a number/index.test.js
@@ -0,0 +1,9 @@
+describe("Solution", function(){
+  it("basic tests", function(){
+    var input = "s2324";
+    Test.assertEquals(isDigit(input), false);
+    
+    input = "-234.4";    
+    Test.assertEquals(isDigit(input), true);    
+  });
+});
