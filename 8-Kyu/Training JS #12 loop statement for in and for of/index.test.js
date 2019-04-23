35235f6f135f6ff2b1d95f2bc969a5eae4bd5c76
dbusii <dtbirkett@gmail.com>
2019-04-23 14:29:51 +0100

    Codewars kata 8-Kyu

---
+++ 8-Kyu/Training JS #12 loop statement for in and for of/index.test.js
@@ -0,0 +1,3 @@
+    Test.assertSimilar(giveMeFive({Our:"earth",is:"a",beautyful:"world"}),["earth","world"]);
+    Test.assertSimilar(giveMeFive({Ihave:"enough", money:"to",buy:"a",car:"model"}),["Ihave","money","model"]);
+    Test.assertSimilar(giveMeFive({Pears:"than",apple:"sweet"}),["Pears","apple","sweet"]);
