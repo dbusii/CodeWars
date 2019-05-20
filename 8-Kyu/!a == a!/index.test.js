5e19054ade0122207e3cc6a126a59b6291d779f1
dbusii <dtbirkett@gmail.com>
2019-05-20 14:58:50 +0100

    Codewars kata 8-Kyu

---
+++ 8-Kyu/!a == a!/index.test.js
@@ -0,0 +1,3 @@
+Test.assertEquals( a == false,  true, `a != false`);
+Test.assertEquals( !a == false, true, `!a != false`);
+Test.assertEquals( a == !a,     true, `a != !a`);
---
+++ 8-Kyu/Basic making six toast/index.test.js
@@ -0,0 +1,3 @@
+Test.assertEquals(sixToast(6), 0);
+Test.assertEquals(sixToast(17), 11);
+Test.assertEquals(sixToast(3), 3);
