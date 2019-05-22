0adaafe94bfa14a27baa3a6a349ab00fc0f155c4
dbusii <dtbirkett@gmail.com>
2019-05-22 16:00:17 +0100

    Codewars kata 8-Kyu

---
+++ 8-Kyu/Alan Partridge II apple turnover/index.test.js
@@ -0,0 +1,4 @@
+Test.describe("Example tests",_=>{
+Test.assertEquals(apple('50'), 'It\'s hotter than the sun!!'); 
+Test.assertEquals(apple(4), 'Help yourself to a honeycomb Yorkie for the glovebox.'); 
+});
